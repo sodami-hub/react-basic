d37bbb7f6d8da67367c9ebb3a0779d24f4bcd134
good4ram@naver.com <good4ram@naver.com>
2025-02-19 12:55:59 +0900

    react-basic ch03_2 init

---
+++ ch03/ch03_2/src/App.tsx
@@ -0,0 +1,15 @@
+import Tailwindcss from './pages/Tailwindcss'
+import Color from './pages/Color'
+import TextsTest from './pages/TextsTest'
+
+function App() {
+  return (
+    <div>
+      <TextsTest />
+      <Color />
+      <Tailwindcss />
+    </div>
+  )
+}
+
+export default App
