773073cdf00dfc4a13c833a4214709310f8b5f00
good4ram@naver.com <good4ram@naver.com>
2025-02-24 13:40:32 +0900

    react-basic ch4_1 init

---
+++ ch04/ch04_1/src/App.tsx
@@ -0,0 +1,13 @@
+import ButtonTest from '../../../ch03/ch03_5/src/pages/ButtonTest'
+import InputTest from '../../../ch03/ch03_5/src/pages/InputTest'
+import ModalTest from '../../../ch03/ch03_5/src/pages/ModalTest'
+
+export default function App() {
+  return (
+    <main>
+      <ButtonTest />
+      <InputTest />
+      <ModalTest />
+    </main>
+  )
+}
